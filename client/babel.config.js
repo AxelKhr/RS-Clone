d771f3376fd47b03c49faae18a039fa11fa7a3e9
Aliaksandr Khramau <axelkhr@gmail.com>
2023-02-07 21:08:00 +0100

    refactor: transition to vue-cli

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,15 +1,3 @@
 module.exports = {
-    presets: [
-        ['@babel/env', {
-            useBuiltIns: 'entry',
-            corejs: 3
-        }],
-        '@babel/typescript'
-    ],
-    plugins: [
-        ['const-enum', {
-            transform: 'constObject'
-        }],
-        '@babel/transform-typescript'
-    ]
+  presets: ["@vue/cli-plugin-babel/preset"],
 };
--- client/webpack.dev.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    mode: 'development',
-    devtool: 'eval-cheap-module-source-map',
-    devServer: {
-        static: path.resolve(__dirname, 'dist'),
-    },
-};
