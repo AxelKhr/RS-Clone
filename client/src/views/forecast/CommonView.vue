e287580a739156cdbb1286454c2faea258dc7f8d
gru6 <iliagrushevich@gmail.com>
2023-02-08 23:42:41 +0300

    fix: pretier problems

--- client/src/views/forecast/CommonView.vue
+++ client/src/views/forecast/CommonView.vue
@@ -20,10 +20,10 @@
 
     <div class="details">
         <div class="block__title">Today</div>
-        <div  class="details__container">
+        <div class="details__container">
             <div v-for="detail in details" :key="detail.id">
-                <div  class="details__subtitle">{{ detail.subtitle }}</div>
-                <div  class="details__value">{{ detail.value }}</div>
+                <div class="details__subtitle">{{ detail.subtitle }}</div>
+                <div class="details__value">{{ detail.value }}</div>
             </div>
         </div>
         <daily-view />
@@ -47,9 +47,8 @@ import { getForecastByLocation } from '../../api/forecast/weather';
 import type LocationForecastResponse from '../../api/types/response';
 export default {
     components: {
-        DailyView
+        DailyView,
     },
-
 
     async setup() {
         const data = await getForecastByLocation({ latitude: 51.5072, longitude: -0.1276 });
@@ -58,16 +57,16 @@ export default {
         const weatherData: LocationForecastResponse = response.data[0];
         weatherData.weather.icon = require(`../../assets/icons/${weatherData.weather.icon}.png`);
         let details = [
-                { id: 0, subtitle: 'Temp', value: '?? Value' },
-                { id: 1, subtitle: 'Humidity', value: `${weatherData.rh} %` },
-                { id: 2, subtitle: 'Precipitation', value: '?? % chance' },
-                { id: 3, subtitle: 'Sunrise', value: `${weatherData.sunrise}` },
-                { id: 4, subtitle: 'Sunset', value: `${weatherData.sunset}` },
-                { id: 5, subtitle: 'Wind', value: `${ weatherData.wind_spd.toFixed(2)} m/s ${weatherData.wind_cdir}` },
-                { id: 6, subtitle: 'Pressure', value: `${Math.round(weatherData.pres / 1.333)} mmHg`},
-                { id: 7, subtitle: 'Clouds Cov.', value: `${weatherData.clouds} %` },
-                { id: 8, subtitle: 'Visibility', value: `${ weatherData.vis } km` },
-            ]
+            { id: 0, subtitle: 'Temp', value: '?? Value' },
+            { id: 1, subtitle: 'Humidity', value: `${weatherData.rh} %` },
+            { id: 2, subtitle: 'Precipitation', value: '?? % chance' },
+            { id: 3, subtitle: 'Sunrise', value: `${weatherData.sunrise}` },
+            { id: 4, subtitle: 'Sunset', value: `${weatherData.sunset}` },
+            { id: 5, subtitle: 'Wind', value: `${weatherData.wind_spd.toFixed(2)} m/s ${weatherData.wind_cdir}` },
+            { id: 6, subtitle: 'Pressure', value: `${Math.round(weatherData.pres / 1.333)} mmHg` },
+            { id: 7, subtitle: 'Clouds Cov.', value: `${weatherData.clouds} %` },
+            { id: 8, subtitle: 'Visibility', value: `${weatherData.vis} km` },
+        ];
         return { weatherData, details };
     },
     methods: {},
