879d20871db3e7c83e8ca7f03072afd7b89c5cae
Artem Pasko <guramibox777@gmail.com>
2023-02-10 00:03:51 +0300

    feat: add simple layers

--- client/src/views/MapView.vue
+++ client/src/views/MapView.vue
@@ -1,14 +1,22 @@
 <template>
     <h2>Map</h2>
-    <div style="height: 80vh; width: 100%">
-        <l-map ref="map" v-model:zoom="zoom" :center="[51.5072, -0.1276]">
+    <div style="height: 85vh; width: 100%">
+        <l-map ref="map" v-model:zoom="zoom" :center="center">
             <l-tile-layer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                layer-type="base"
-                name="OpenStreetMap"
-            ></l-tile-layer>
-            <l-grid-layer :child-render="childRender"></l-grid-layer>
+                v-for="tileProvider in tileProviders"
+                :key="tileProvider.name"
+                :name="tileProvider.name"
+                :url="tileProvider.url"
+                :layer-type="tileProvider.layerType"/>
+            <l-tile-layer
+                v-for="weatherTile in weatherTiles"
+                :key="weatherTile.name"
+                :name="weatherTile.name"
+                :url="weatherTile.url"
+                :visible="weatherTile.visible"
+                :layer-type="weatherTile.layerType"/>
             <l-control-layers />
+            <l-grid-layer :child-render="childRender"></l-grid-layer>
             <l-geo-json :geojson="geojson"></l-geo-json>
         </l-map>
     </div>
@@ -29,26 +37,51 @@ export default {
     },
     data() {
         return {
-            //http://maps.weatherbit.io/v2.0/singleband/{field}/{time}/{z}/{x}/{y}.png?key=a23d71951d4f4dd89ced2ad48627f1c1
+            center: [51.5072, -0.1276],
             zoom: 6,
+            tileProviders: [
+                {
+                    url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    temp_url: 'https://tile.openweathermap.org/map/pressure_new/{z}/{x}/{y}.png?appid=1d44eaad1808dee6a14e9f7c1db9bdf4',
+                    name: 'OpenMap',
+                    layerType: 'base',
+                },
+            ],
+            weatherTiles: [
+                {
+                    url: 'https://tile.openweathermap.org/map/pressure_new/{z}/{x}/{y}.png?appid=1d44eaad1808dee6a14e9f7c1db9bdf4',
+                    name: 'Pressure',
+                    layerType: 'overlay',
+                    visible: false,
+                },
+                {
+                    url: 'https://tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=1d44eaad1808dee6a14e9f7c1db9bdf4',
+                    name: 'Clouds',
+                    layerType: 'overlay',
+                    visible: false,
+                },
+                {
+                    url: 'https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=1d44eaad1808dee6a14e9f7c1db9bdf4',
+                    name: 'Precipitation',
+                    layerType: 'overlay',
+                    visible: false,
+                },
+                {
+                    url: 'https://tile.openweathermap.org/map/wind_new/{z}/{x}/{y}.png?appid=1d44eaad1808dee6a14e9f7c1db9bdf4',
+                    name: 'Wind',
+                    layerType: 'overlay',
+                    visible: false,
+                },
+            ],
             childRender: (props) => () => {
                 return h(
                     'div',
                     { style: 'border: 1px solid grey; height: 100%;' },
-                    `x: ${props.coords.x} y: ${props.coords.y} z: ${props.coords.z}`
+                    `x: ${props.coord.x} y: ${props.coords.y} z: ${props.coords.z}`
                 );
             },
             geojson: null,
         };
-    },
-    async created() {
-        const response = await fetch(
-            'https://tile.openweathermap.org/map/pressure_new/10/3/31.png?appid=1d44eaad1808dee6a14e9f7c1db9bdf4'
-            //'http://maps.weatherbit.io/v2.0/singleband/{field}/{time}/{z}/{x}/{y}.png?key=a23d71951d4f4dd89ced2ad48627f1c1'
-            //'http://maps.openweathermap.org/maps/2.0/weather/TA2/{z}/{x}/{y}?date=1527811200&opacity=0.9&fill_bound=true&appid={API key}'
-            //'https://rawgit.com/gregoiredavid/france-geojson/master/regions/pays-de-la-loire/communes-pays-de-la-loire.geojson'
-        );
-        this.geojson = await response.json();
     },
 };
 </script>
