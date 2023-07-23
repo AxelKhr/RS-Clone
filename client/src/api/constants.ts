a3d507bc705b57d00dd9204ebf5da7cdf76c860d
gru6 <iliagrushevich@gmail.com>
2023-07-23 13:39:13 +0300

    feat: new Api

--- client/src/api/constants.ts
+++ client/src/api/constants.ts
@@ -1,9 +1,6 @@
-export const API_KEY = "e2a8e8d44b3640699de25c729a697f0c";
+export const API_KEY = '56fb8f6250434970a7fb9f245db69028';
 
-export const OSM_MAP_BASE_URL =
-  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-export const CARTO_MAP_DARK_URL =
-  "http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png";
-export const CARTO_MAP_LIGHT_URL =
-  "http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png";
+export const OSM_MAP_BASE_URL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+export const CARTO_MAP_DARK_URL = 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png';
+export const CARTO_MAP_LIGHT_URL = 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png';
 export const MAP_OVERLAY_URL = `https://tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?appid=1d44eaad1808dee6a14e9f7c1db9bdf4`;
