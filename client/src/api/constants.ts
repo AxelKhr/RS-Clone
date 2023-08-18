b75666a928dbefcbc7d23b117f4b5b0049a945e2
gru6 <iliagrushevich@gmail.com>
2023-08-18 23:23:26 +0300

    feat: add new api key

--- client/src/api/constants.ts
+++ client/src/api/constants.ts
@@ -1,6 +1,9 @@
-export const API_KEY = '56fb8f6250434970a7fb9f245db69028';
+export const API_KEY = "e002af3c85e54a40a5b02dbf01aa16d7";
 
-export const OSM_MAP_BASE_URL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-export const CARTO_MAP_DARK_URL = 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png';
-export const CARTO_MAP_LIGHT_URL = 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png';
+export const OSM_MAP_BASE_URL =
+  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+export const CARTO_MAP_DARK_URL =
+  "http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png";
+export const CARTO_MAP_LIGHT_URL =
+  "http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png";
 export const MAP_OVERLAY_URL = `https://tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?appid=1d44eaad1808dee6a14e9f7c1db9bdf4`;
